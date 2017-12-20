a8529be864217ad82e39e6d4363e1b13a1b67ef2
Sambhav Sunkerneni <sambhav.reddy@gmail.com>
2017-12-20 13:25:40 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 #Okpy Grade Calculator for CS61a Fall 2017
 #
 #Note: Make Sure To Use Python3
+#Requires Beautiful Soup & the lxml parser. You can install both very easily with the instructions here:
+#https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
 #
 #Instructions: 
 #Please save your okpy html page as an html document and put it in this folder.
