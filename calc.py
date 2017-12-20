bbf8d2b3dbd205508b3a47506d5663628c333068
Sambhav Sunkerneni <sambhav.reddy@gmail.com>
2017-12-20 14:20:13 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
-###
-#Okpy Grade Calculator for CS61a Fall 2017
-#
-#Note: Make Sure To Use Python3
-#
-#Instructions: 
-#Please save your okpy html page as an html document and put it in this folder.
-#Make sure it is saved as 'index.html'
-#Run the program by calling | $python3 calc.py | in your terminal.
-#
-#Made by Sambhav Sunkerneni
-###
+"""
+Okpy Grade Calculator for CS61a Fall 2017
+
+Note: Make Sure To Use Python3
+
+Instructions: 
+Please save your okpy html page as an html document and put it in this folder.
+Make sure it is saved as 'index.html'
+Run the program by calling | $python3 calc.py | in your terminal.
+
+Made by Sambhav Sunkerneni
+"""
 
 #Initializations
 participation, hw, midterm, contest_ec, projects = 0, 0, 0, 0, 0
